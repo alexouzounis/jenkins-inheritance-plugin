b8caacab15701e09f3531b2aaa734814783bf13f
Alex Ouzounis <alex.ouzounis@me.com>
2014-08-09 16:56:19 +0100

    Reverted JobViewInheritanceChecker
    added "HTML_REPORT_.*" group to match html reports

--- src/main/java/hudson/plugins/project_inheritance/projects/inheritance/JobViewInheritanceChecker.java
+++ src/main/java/hudson/plugins/project_inheritance/projects/inheritance/JobViewInheritanceChecker.java
@@ -12,15 +12,51 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Extension
-public class JobViewInheritanceChecker extends RequestInheritanceChecker {
+ public class JobViewInheritanceChecker extends RequestInheritanceChecker {
+ 
+    private static final String JOB_MAIN_VIEW_REGEX        = ".*/job/[^/]+/";
 
-    /**
-     * Checks if given URI is job main page or one of its specified subpages.
-     */
+    private static final String JOB_RUN_PAGE_REGEX         = "[0-9]+/.*";
+
+    private static final String TEST_TREND_GRAPH_REGEX     = "test/trend";
+
+    private static final String FINDBUGS_TREND_GRAPH_REGEX = "findbugs/trendGraph/png";
+
+    private static final String JACOCO_TREND_GRAPH_REGEX   = "jacoco/graph";
+    
+    private static final String WARNINGS_GRAPH_REGEX   = "warnings.*";
+
+    private static final String HTML_REPORT_REGEX   = "HTML_REPORT_.*";
+
+    public static final Pattern jobUriRegExp;
+
+    static {
+        StringBuilder regExpBuilder = new StringBuilder();
+        regExpBuilder.append(JOB_MAIN_VIEW_REGEX);
+        regExpBuilder.append("(");
+        regExpBuilder.append(JOB_RUN_PAGE_REGEX);
+        regExpBuilder.append("|");
+        regExpBuilder.append(TEST_TREND_GRAPH_REGEX);
+        regExpBuilder.append("|");
+        regExpBuilder.append(FINDBUGS_TREND_GRAPH_REGEX);
+        regExpBuilder.append("|");
+        regExpBuilder.append(JACOCO_TREND_GRAPH_REGEX);
+        regExpBuilder.append("|");
+        regExpBuilder.append(WARNINGS_GRAPH_REGEX);
+        regExpBuilder.append("|");
+        regExpBuilder.append(HTML_REPORT_REGEX);
+        regExpBuilder.append(")?");
+        jobUriRegExp = Pattern.compile(regExpBuilder.toString());
+    }
+
+     /**
+      * Checks if given URI is job main page or one of its specified subpages.
+      */
     @Override
-    public boolean isInheritanceRequired(String requestURI) {
+     public boolean isInheritanceRequired(String requestURI) {
+        Matcher matcher = jobUriRegExp.matcher(requestURI);
+        return matcher.matches();
         // just allow everything
         return true;
     }
-
 }
