9e9f7fb662832a94160e9c0df153ac12acd904dc
Alex Ouzounis <alex.ouzounis@me.com>
2014-08-09 17:27:14 +0100

    allow all job inheritance

--- src/main/java/hudson/plugins/project_inheritance/projects/inheritance/JobViewInheritanceChecker.java
+++ src/main/java/hudson/plugins/project_inheritance/projects/inheritance/JobViewInheritanceChecker.java
@@ -13,56 +13,12 @@ import java.util.regex.Pattern;
 
 @Extension
  public class JobViewInheritanceChecker extends RequestInheritanceChecker {
- 
-    private static final String JOB_MAIN_VIEW_REGEX        = ".*/job/[^/]+/";
-
-    private static final String JOB_RUN_PAGE_REGEX         = "[0-9]+/.*";
-
-    private static final String TEST_TREND_GRAPH_REGEX     = "test/trend";
-
-    private static final String FINDBUGS_TREND_GRAPH_REGEX = "findbugs/trendGraph/png";
-
-    private static final String JACOCO_TREND_GRAPH_REGEX   = "jacoco/graph";
-    
-    private static final String WARNINGS_GRAPH_REGEX   = "warnings.*";
-
-    private static final String HTML_REPORT_REGEX   = "HTML_REPORT_.*";
-
-    private static final String RELEASE_REGEX   = "release";
-
-    private static final String EMAIL_TEMPLATE_TEST_REGEX   = "templateTest";
-
-    public static final Pattern jobUriRegExp;
-
-    static {
-        StringBuilder regExpBuilder = new StringBuilder();
-        regExpBuilder.append(JOB_MAIN_VIEW_REGEX);
-        regExpBuilder.append("(");
-        regExpBuilder.append(JOB_RUN_PAGE_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(TEST_TREND_GRAPH_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(FINDBUGS_TREND_GRAPH_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(JACOCO_TREND_GRAPH_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(WARNINGS_GRAPH_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(HTML_REPORT_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(RELEASE_REGEX);
-        regExpBuilder.append("|");
-        regExpBuilder.append(EMAIL_TEMPLATE_TEST_REGEX);
-        regExpBuilder.append(")?");
-        jobUriRegExp = Pattern.compile(regExpBuilder.toString());
-    }
 
      /**
       * Checks if given URI is job main page or one of its specified subpages.
       */
     @Override
      public boolean isInheritanceRequired(String requestURI) {
-        Matcher matcher = jobUriRegExp.matcher(requestURI);
-        return matcher.matches();
+		return true;
     }
 }
